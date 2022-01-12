abacf1bdba51f4fbfac2b2efea0c58635495908b
Alani Johnson <alani.johnson@smartsheet.com>
2022-01-12 16:25:40 -0600

    Exercise 6 extra credit 2. useState

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -5,6 +5,7 @@ import * as React from 'react'
 
 function UsernameForm({onSubmitUsername}) {
     const usernameRef = React.useRef(null);
+    const [errorState, setErrorState] = React.useState(false);
   function handleSubmit(event) {
       event.preventDefault();
       const value = usernameRef.current.value;
@@ -12,14 +13,27 @@ function UsernameForm({onSubmitUsername}) {
       event.target.elements.username.value = "";
   }
 
+  function handleChange(event) {
+      if (event.target.value.toLowerCase() !== event.target.value) {
+          setErrorState(true);
+      } else {
+          setErrorState(false);
+      }
+  }
+
+  const Error = (props) => {
+      const visibility = errorState ? "visible" : "hidden";
+      return <div style={{visibility: visibility, color:'red'}}>{props.children}</div>
+  }
 
   return (
     <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor="username">Username:</label>
-        <input ref={usernameRef} type="text" id="username" />
+        <input ref={usernameRef} type="text" id="username" onChange={handleChange}/>
       </div>
-      <button type="submit">Submit</button>
+      <button type="submit" disabled={errorState}>Submit</button>
+        <Error>"The text must be lowercase"</Error>
     </form>
   )
 }
