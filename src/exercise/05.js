9a17a5097681de2242f67f3d4f147fe813d34fa0
Alani Johnson <alani.johnson@smartsheet.com>
2022-01-12 14:01:52 -0600

    Exercise 5 problem 1

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -14,9 +14,9 @@ import '../box-styles.css'
 // 🐨 also use the style prop to make the font italic
 // 💰 Here are available style attributes: backgroundColor, fontStyle
 
-const smallBox = <div>small lightblue box</div>
-const mediumBox = <div>medium pink box</div>
-const largeBox = <div>large orange box</div>
+const smallBox = <div className="box--small box">small lightblue box</div>
+const mediumBox = <div className="box--medium box">medium pink box</div>
+const largeBox = <div className="box--large box">large orange box</div>
 
 function App() {
   return (
